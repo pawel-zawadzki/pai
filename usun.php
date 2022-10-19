96471a22693a6ff817237ede453608c25e9294b4
Paweł Zawadzki <pawel.zawadzki@solutionslab.pl>
2022-10-19 19:14:22 +0200

    zmiana koloru

--- usun.php
+++ usun.php
@@ -5,13 +5,13 @@ require_once 'funkcje.php';
 sprawdzLogowanie();
 
 if (isset($_GET['id'])) {
-	$pdo = polacz();
-	$stmt = $pdo->prepare("DELETE FROM samochody WHERE id = :id");
-	$wynik = $stmt->execute(['id' => $_GET['id']]);
+    $pdo = polacz();
+    $stmt = $pdo->prepare("DELETE FROM samochody WHERE id = :id");
+    $wynik = $stmt->execute(['id' => $_GET['id']]);
 
-	if ($wynik == true) {
-		header("Location: index.php?komunikat=2");
-	} else {
-		echo "<p style='color:red; font-weight:bold;'>Usuwanie nie powiodlo sie.</p>";
-	}
+    if ($wynik == true) {
+        header("Location: index.php?komunikat=2");
+    } else {
+        echo "<p style='color:green; font-weight:bold;'>Usuwanie nie powiodlo sie.</p>";
+    }
 }
